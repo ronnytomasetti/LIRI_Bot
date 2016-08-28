7e10ce3d14e75fd72b09c49219af021213d38914
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-08-28 14:45:51 -0400

    Updated Spotify api client used.

--- liri.js
+++ liri.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var os = require('os');
 var request = require('request');
 var Twitter = require('twitter');
-var spotify = require('spotify');
+var Spotify = require('spotify-web-api-node');
+var prettyjson = require('prettyjson');
 var keys = require('./keys.js');
 
 var first_argv = process.argv[2];
@@ -12,8 +13,8 @@ var second_argv = process.argv[3];
 * Determine which command to run based on arguments provided or respond with error
 * message if argument is not supported.
 *
-* @param {string} cmd LIRI command user wants to execute.
-* @param {string} param If needed, argument value provided by user (ie, song or movie).
+* @param {String} cmd LIRI command user wants to execute.
+* @param {String} param If needed, argument value provided by user (ie, song or movie).
 * @return {}
 */
 function liriCommandRunner(cmd, param) {
@@ -61,16 +62,33 @@ function myTweets() {
     var user = 'RonnyTomasetti';
     var tweet_count = 20;
 
-    twitter_client.get('statuses/user_timeline', {screen_name: user, count: tweet_count}, function(error, tweets, response) {
+    twitter_client.get('statuses/user_timeline', {screen_name: user, count: tweet_count}, function(error, tweets) {
 
-        if(error)
+        if (error)
             throw error;
         else {
-            console.log("**********************************************");
-            console.log("TWEETS: ", tweets);
-            console.log("**********************************************");
-            console.log( "RESPONSE", response);
-            console.log("**********************************************");
+            var data = [];
+            var log_options = {
+                    keysColor  : 'green',
+                    dashColor  : 'green',
+                    stringColor: 'white'
+                    };
+
+            for ( i in tweets ) {
+                var item = {
+                        "Created"  : tweets[i].created_at,
+                        "Tweet"    : tweets[i].text,
+                        "Retweeted": tweets[i].retweet_count,
+                        "Favorited": tweets[i].favorite_count
+                        };
+                data.push(item);
+            }
+
+            console.log("------------------------------------------------------------");
+            console.log("Successfully retrieved " + tweets.length + " tweets [ max 20 ] from Twitter.com");
+            console.log("------------------------------------------------------------");
+            console.log(prettyjson.render(data, log_options));
+            console.log("------------------------------------------------------------");
         }
     });
 }
@@ -84,22 +102,29 @@ function myTweets() {
 * artist(s), preview link for Spotify. If no song is provided, default will
 * display data for "The Sign" by Ace of Base.
 *
-* @param {string} song Title of song to query using Spotify API.
+* @param {String} song Title of song to query using Spotify API.
 * @return {}
 */
 function spotifyThis(song) {
 
-    spotify.search({ type: 'track', query: song }, function(error, data) {
+    var spotify_client = new Spotify({
+        clientId : '3ca39f9c22294dde999f49f6d129c02e',
+        clientSecret : '5cc62e1dbe1847d4bdb0eb340d6ea7eb'
+    });
 
-        if (error) {
-            console.log('Error occurred: ' + error);
-            return;
-        }
-        else {
-            console.log("**********************************************");
-            console.log("Spotify Data: ", data);
-            console.log("**********************************************");
-        }
+    spotify_client.searchTracks(song).then( function(data) {
+
+        console.log('I got ' + data.body.tracks.total + ' results!');
+
+        var firstPage = data.body.tracks.items;
+        console.log('The tracks in the first page are.. (popularity in parentheses)');
+
+        firstPage.forEach(function(track, index) {
+            console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
+        });
+
+    }, function(error) {
+            console.error(error);
     });
 }
 
@@ -114,16 +139,16 @@ function spotifyThis(song) {
 *
 * If no movie specified, output data for the movie 'Mr. Nobody'.
 *
-* @param {string} movie Title of movie to query using OMDB API.
+* @param {String} movie Title of movie to query using OMDB API.
 * @return {}
 */
 function movieThis(movie) {
 
-    var queryUrl = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&r=json';
+    var query_url = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&r=json';
 
-    console.log(queryUrl);
+    console.log(query_url);
 
-    request(queryUrl, function(error, res, body) {
+    request(query_url, function(error, res, body) {
 
         if (!error && res.statusCode == 200) {
             console.log("MOVIE DATA: ", JSON.parse(body))
@@ -147,8 +172,8 @@ function doWhatItSays() {
 
         var ran_txt = random_txt.split(',');
 
-        for(index in ran_txt)
-            console.log("Random Text File: ", ran_txt[index]);
+        for ( i in ran_txt)
+            console.log("Random Text File: ", ran_txt[i]);
     });
 
     var log_entry = "Ran do-what-it-says command";
@@ -166,8 +191,8 @@ function appendLogFile(log_entry) {
 
     fs.appendFile('log.txt', log_entry + os.EOL, 'utf8', function(error) {
 
-        if(error)
-            console.log(error);
+        if (error)
+            throw error;
         else
             console.log("-- Logged --")
     });
